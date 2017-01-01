009d0547d949d51d3b9819d22001504a37befadf
Sebastian Bank <sebastian.bank@uni-leipzig.de>
2017-01-01 13:25:35 +0100

    tag python 3.6 support, 2017

--- docs/conf.py
+++ docs/conf.py
@@ -54,7 +54,7 @@ master_doc = 'index'
 
 # General information about the project.
 project = u'gsheets'
-copyright = u'2016, Sebastian Bank'
+copyright = u'2016-2017, Sebastian Bank'
 author = u'Sebastian Bank'
 
 # The version info for the project you're documenting, acts as replacement for
