e35306a6e743025394e8b32abfe260bbe4017d2f
Sebastian Bank <sebastian.bank@uni-leipzig.de>
2017-06-06 13:09:03 +0200

    update test script workarounds

--- run-tests.py
+++ run-tests.py
@@ -2,7 +2,6 @@
 # run-tests.py
 
 import sys
-import platform
 
 import pytest
 
@@ -11,10 +10,9 @@ ARGS = [
     #'--pdb',
 ]
 
-if 'idlelib' in sys.modules:
-    ARGS.append('--color=no')
-
-if platform.system().lower() == 'windows':
+if 'idlelib' in sys.modules or 'thonny' in sys.modules:
+    ARGS.extend(['--capture=sys', '--color=no'])
+elif sys.version_info[0] == 2 and 'win_unicode_console' in sys.modules:
     ARGS.append('--capture=sys')
 
 pytest.main(ARGS + sys.argv[1:])
