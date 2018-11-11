7738a79309afbc7700347ffef1895700448b0500
Sebastian Bank <sebastian.bank@uni-leipzig.de>
2018-11-11 21:58:56 +0100

    blacklist pytest 3.10.0 (minversion bug)

--- setup.py
+++ setup.py
@@ -21,7 +21,7 @@ setup(
     ],
     extras_require={
         'dev': ['tox>=3.0', 'flake8', 'pep8-naming', 'wheel', 'twine'],
-        'test': ['mock>=2', 'pytest>=3.4', 'pytest-mock>=1.8', 'pytest-cov'],
+        'test': ['mock>=2', 'pytest>=3.4,!=3.10.0', 'pytest-mock>=1.8', 'pytest-cov'],
         'docs': ['sphinx>=1.7', 'sphinx-rtd-theme'],
     },
     long_description=io.open('README.rst', encoding='utf-8').read(),
