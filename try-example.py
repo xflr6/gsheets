f4c8bb037c1e17f2aa902be503d110b97f36a47a
Sebastian Bank <sebastian.bank@uni-leipzig.de>
2019-12-10 09:12:29 +0100

    use dedicated storage.json file in home dir for live testing

--- try-example.py
+++ try-example.py
@@ -15,7 +15,7 @@ WORKSHEET_NAMES = ['Tabellenblatt1', 'Tabellenblatt2', 'Tabellenblatt3']
 logging.basicConfig(format='[%(levelname)s@%(name)s] %(message)s',
                     level=logging.INFO)
 
-sheets = Sheets.from_files('~/client_secrets.json', 'storage.json')
+sheets = Sheets.from_files('~/client_secrets.json', '~/gsheets-storage.json')
 
 print(sheets[SHEET_ID])
 
