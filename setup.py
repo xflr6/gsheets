9234b0d057afb8a76c6758f0e89fb4886671334d
Sebastian Bank <sebastian.bank@uni-leipzig.de>
2020-03-18 16:21:13 +0100

    google-api-python-client 1.8 (1.7.12 on Python 2)

--- setup.py
+++ setup.py
@@ -21,7 +21,8 @@ setup(
     platforms='any',
     python_requires='>=2.7,!=3.0.*,!=3.1.*,!=3.2.*,!=3.3.*,!=3.4.*',
     install_requires=[
-        'google-api-python-client',
+        'google-api-python-client<1.8; python_version < "3"',
+        'google-api-python-client; python_version >= "3"',
         'oauth2client>=1.5.0',
     ],
     extras_require={
