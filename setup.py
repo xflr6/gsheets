5a670750c45252d27ffdae1a9b99d19d2f69866b
Sebastian Bank <sebastian.bank@uni-leipzig.de>
2020-05-11 12:28:03 +0200

    upgrade test and docs extra dependencies

--- setup.py
+++ setup.py
@@ -26,9 +26,9 @@ setup(
         'oauth2client>=1.5.0',
     ],
     extras_require={
-        'dev': ['tox>=3.0', 'flake8', 'pep8-naming', 'wheel', 'twine'],
-        'test': ['mock>=2', 'pytest>=3.4,!=3.10.0', 'pytest-mock>=1.8', 'pytest-cov'],
-        'docs': ['sphinx>=1.7', 'sphinx-rtd-theme'],
+        'dev': ['tox>=3', 'flake8', 'pep8-naming', 'wheel', 'twine'],
+        'test': ['mock>=3', 'pytest>=4', 'pytest-mock>=2', 'pytest-cov'],
+        'docs': ['sphinx>=1.8', 'sphinx-rtd-theme'],
     },
     long_description=io.open('README.rst', encoding='utf-8').read(),
     classifiers=[
