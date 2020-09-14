3dca39aa10d79b4b6811574b184c6a7f5d0089c0
Sebastian Bank <sebastian.bank@uni-leipzig.de>
2020-09-14 18:38:33 +0200

    drop Python 3.5 support

--- setup.py
+++ setup.py
@@ -19,7 +19,7 @@ setup(
     },
     packages=find_packages(),
     platforms='any',
-    python_requires='>=2.7,!=3.0.*,!=3.1.*,!=3.2.*,!=3.3.*,!=3.4.*',
+    python_requires='>=2.7,!=3.0.*,!=3.1.*,!=3.2.*,!=3.3.*,!=3.4.*,!=3.5.*',
     install_requires=[
         'google-api-python-client<1.8; python_version < "3"',
         'google-api-python-client; python_version >= "3"',
@@ -40,7 +40,6 @@ setup(
         'Programming Language :: Python :: 2',
         'Programming Language :: Python :: 2.7',
         'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
         'Programming Language :: Python :: 3.8',
