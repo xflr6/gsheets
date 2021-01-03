4dc0ca094feca73eca5b8885ab59bc15250427c6
Sebastian Bank <sebastian.bank@uni-leipzig.de>
2021-01-03 23:14:44 +0100

    bump version for dropping Python 2 support

--- docs/conf.py
+++ docs/conf.py
@@ -21,11 +21,11 @@ import gsheets
 # -- Project information -----------------------------------------------------
 
 project = 'gsheets'
-copyright = '2016-2020, Sebastian Bank'
+copyright = '2016-2021, Sebastian Bank'
 author = 'Sebastian Bank'
 
 # The short X.Y version
-version = '0.5.2.dev0'
+version = '0.6.dev0'
 # The full version, including alpha/beta/rc tags
 release = version
 
--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='gsheets',
-    version='0.5.2.dev0',
+    version='0.6.dev0',
     author='Sebastian Bank',
     author_email='sebastian.bank@uni-leipzig.de',
     description='Pythonic wrapper for the Google Sheets API',
@@ -39,8 +39,6 @@ setup(
         'Intended Audience :: Science/Research',
         'License :: OSI Approved :: MIT License',
         'Operating System :: OS Independent',
-        'Programming Language :: Python :: 2',
-        'Programming Language :: Python :: 2.7',
         'Programming Language :: Python :: 3',
         'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
