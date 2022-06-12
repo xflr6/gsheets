430adb555685816e8ceef731bff723737f6a0b93
Sebastian Bank <sebastian.bank@uni-leipzig.de>
2022-06-12 13:44:09 +0200

    bump version for development

--- docs/conf.py
+++ docs/conf.py
@@ -23,7 +23,7 @@ copyright = '2016-2022, Sebastian Bank'
 author = 'Sebastian Bank'
 
 # The full version, including alpha/beta/rc tags
-release = '0.6.1'
+release = '0.6.2.dev0'
 
 
 # -- General configuration ---------------------------------------------------
--- gsheets/__init__.py
+++ gsheets/__init__.py
@@ -9,7 +9,7 @@ from .oauth2 import get_credentials
 __all__ = ['Sheets', 'get_credentials', 'build_service']
 
 __title__ = 'gsheets'
-__version__ = '0.6.1'
+__version__ = '0.6.2.dev0'
 __author__ = 'Sebastian Bank <sebastian.bank@uni-leipzig.de>'
 __license__ = 'MIT, see LICENSE.txt'
 __copyright__ = 'Copyright (c) 2016-2022 Sebastian Bank'
